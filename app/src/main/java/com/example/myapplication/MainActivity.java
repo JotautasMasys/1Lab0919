56c252f8b9a28c1a245a0e574037468edf0056e7
JotautasMasys <mjotautas25@gmail.com>
2023-10-10 18:33:33 +0300

    Pridėtas teksto pakeitimo funkciją į sistemą.

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -3,12 +3,23 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    TextView tvMain;
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        this.tvMain = findViewById(R.id.tvMain);
+    }
+
+    public void OnButtonChangeTextClick(View view) {
+
+        this.tvMain.setText("Labas pasauli!");
     }
 }
